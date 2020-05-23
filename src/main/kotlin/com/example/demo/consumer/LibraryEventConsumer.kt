787b87e25c280f96b380a90838a4befe49ac2ba2
tatitati <francis.albusac@simplybusiness.co.uk>
2020-05-23 20:59:46 +0100

    Sat May 23 20:59:46 BST 2020

--- src/main/kotlin/com/example/demo/consumer/LibraryEventConsumer.kt
+++ src/main/kotlin/com/example/demo/consumer/LibraryEventConsumer.kt
@@ -1,15 +1,8 @@
 package com.example.demo.consumer
 
-import com.fasterxml.jackson.core.JsonProcessingException
-import com.fasterxml.jackson.databind.ObjectMapper
 import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.apache.kafka.clients.producer.ProducerRecord
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.kafka.core.KafkaTemplate
-import org.springframework.kafka.support.SendResult
 import org.springframework.stereotype.Component
-import org.springframework.util.concurrent.ListenableFutureCallback
 
 @Component
 class LibraryEventConsumer {
