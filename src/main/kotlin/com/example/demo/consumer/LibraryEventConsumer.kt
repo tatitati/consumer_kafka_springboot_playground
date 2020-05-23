49dc28367f6feb5987d106dad35b94c53d44e94a
tatitati <francis.albusac@simplybusiness.co.uk>
2020-05-23 20:49:52 +0100

    Sat May 23 20:49:52 BST 2020

--- src/main/kotlin/com/example/demo/consumer/LibraryEventConsumer.kt
+++ src/main/kotlin/com/example/demo/consumer/LibraryEventConsumer.kt
@@ -3,8 +3,10 @@ package com.example.demo.consumer
 import com.example.demo.domain.LibraryEvent
 import com.fasterxml.jackson.core.JsonProcessingException
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.support.SendResult
 import org.springframework.stereotype.Component
@@ -12,5 +14,9 @@ import org.springframework.util.concurrent.ListenableFutureCallback
 
 @Component
 class LibraryEventConsumer {
-    
+
+    @KafkaListener(topics = arrayOf("library-events"))
+    fun myconsumer(consumerRecord: ConsumerRecord<Int, String>) {
+        
+    }
 }
--- src/main/kotlin/com/example/demo/ui/EventsController.kt
+++ src/main/kotlin/com/example/demo/ui/EventsController.kt
@@ -1,12 +1,7 @@
 package com.example.demo.ui
 
-import com.example.demo.domain.LibraryEvent
-import com.example.demo.producer.LibraryEventProducer
+import com.example.demo.consumer.LibraryEventProducer
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 
