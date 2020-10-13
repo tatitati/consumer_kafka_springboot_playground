961dce6a8e441daff3ba34d790457aa5f53c430e
tatitati <francis.albusac@simplybusiness.co.uk>
2020-10-13 22:53:16 +0100

    renamed things

--- src/test/kotlin/producer/BasicProducerWithTemplate.kt
+++ src/test/kotlin/producer/BasicProducerWithTemplate.kt
@@ -7,19 +7,19 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory
 import org.springframework.kafka.core.KafkaTemplate
 
 class BasicProducerWithTemplate {
-    fun producerTemplate(): KafkaTemplate<String?, String?> {
+    fun kafkaTemplate(): KafkaTemplate<String?, String?> {
 		val props: MutableMap<String, String> = HashMap()
 		props[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = "localhost:9092"
 		props[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = "org.apache.kafka.common.serialization.StringSerializer"
 		props[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = "org.apache.kafka.common.serialization.StringSerializer"
 
-		val factory: DefaultKafkaProducerFactory<String, String> = DefaultKafkaProducerFactory(props as Map<String, Any>)
-		return KafkaTemplate(factory)
+		val producer: DefaultKafkaProducerFactory<String, String> = DefaultKafkaProducerFactory(props as Map<String, Any>)
+		return KafkaTemplate(producer)
 	}
 
 	@Test
 	fun `can producer msgs to a topic`(){
-		producerTemplate()
+		kafkaTemplate()
 				.send(
                         ProducerRecord<String?, String?>("spring", "this is the msgggg again"))
 
